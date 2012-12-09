bcdfdf4e5d40b2b5da00bd9b1fcde0db2fbb9b63
Ryan Sullivan <KayoticSully@gmail.com>
2012-12-09 02:57:58 -0500

    Added comment for Downloads

--- html/application/views/home.php
+++ html/application/views/home.php
@@ -10,6 +10,9 @@
     <h1>
         Downloads
     </h1>
+    <code class="comment">
+        // Downloads will be grouped by type and look similar to the announcements 
+    </code>
     <div>
         <?php foreach($downloads as $download): ?>
             <?php echo $download->name; ?>
