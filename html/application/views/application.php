348f51fc9a8a65be1a3fdcbb2a0edcce4b509857
Ryan Sullivan <KayoticSully@gmail.com>
2012-12-10 21:22:21 -0500

    Menu tweaks

--- html/application/views/application.php
+++ html/application/views/application.php
@@ -16,20 +16,18 @@
             <i class="icon-group"></i>
             Community
         </li>
-        <li id="login_wrapper">
-            <div id="login_form">
-                <div class="nav_form_label">
-                    <i class="icon-user"></i>
-                    Login
-                </div>
-                <div class="login_section">
-                    <input type="text" placeholder="Username" />
-                    <input type="password" placeholder="Password" />
-                </div>
-                <div class="login_section">
-                    <input type="submit" value="Login" class="btn btn-primary" />
-                    <a class="btn btn-link" target="_blank" href="http://minecraft.net/resetpassword">Lost Password?</a>
-                </div>
+        <li id="login_form">
+            <div class="nav_form_label">
+                <i class="icon-user"></i>
+                Login
+            </div>
+            <div class="login_section">
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+            </div>
+            <div class="login_section">
+                <input type="submit" value="Login" class="btn btn-primary" />
+                <a class="btn btn-link" target="_blank" href="http://minecraft.net/resetpassword">Lost Password?</a>
             </div>
         </li>
     </ul>
