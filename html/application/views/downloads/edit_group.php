c8f77bb547023c2abaca90ace23cb20e6375125f
Ryan Sullivan <KayoticSully@gmail.com>
2012-12-14 23:22:18 -0500

    Initial group form now displays

--- html/application/views/downloads/edit_group.php
+++ html/application/views/downloads/edit_group.php
@@ -1,3 +1,25 @@
-<h2>
-    <?php echo $group->name ?>
-</h2>
+<form class="form-horizontal">
+    <fieldset>
+        <legend>
+            <?php echo $group->name; ?>
+        </legend>
+        <div class="control-group">
+            <label class="control-label" for="rename">Rename</label>
+            <div class="controls">
+                <input type="text" id="rename" name="rename" placeholder="<?php echo $group->name; ?>" />
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="version">Version</label>
+            <div class="controls">
+                <input type="text" id="version" name="version" value="<?php echo $group->version; ?>" />
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="description">Description</label>
+            <div class="controls">
+                <textarea rows="5" id="description" name="description"><?php echo $group->description; ?></textarea>
+            </div>
+        </div>
+    </fieldset>
+</form>
