0ca21bb34f99e032c83a39b8d421dc56c2c1229c
Ryan Sullivan <KayoticSully@gmail.com>
2012-12-14 23:58:36 -0500

    Download files are now listed for each group

--- html/application/views/downloads/edit_group.php
+++ html/application/views/downloads/edit_group.php
@@ -1,25 +1,62 @@
-<form class="form-horizontal">
-    <fieldset>
-        <legend>
-            <?php echo $group->name; ?>
-        </legend>
-        <div class="control-group">
-            <label class="control-label" for="rename">Rename</label>
-            <div class="controls">
-                <input type="text" id="rename" name="rename" placeholder="<?php echo $group->name; ?>" />
+<section>
+    <form class="form-horizontal">
+        <fieldset>
+            <legend>
+                <?php echo $group->name; ?>
+            </legend>
+            <div class="control-group">
+                <label class="control-label" for="rename">Rename</label>
+                <div class="controls">
+                    <input type="text" id="rename" name="rename" placeholder="<?php echo $group->name; ?>" />
+                </div>
             </div>
-        </div>
-        <div class="control-group">
-            <label class="control-label" for="version">Version</label>
-            <div class="controls">
-                <input type="text" id="version" name="version" value="<?php echo $group->version; ?>" />
+            <div class="control-group">
+                <label class="control-label" for="version">Version</label>
+                <div class="controls">
+                    <input type="text" id="version" name="version" value="<?php echo $group->version; ?>" />
+                </div>
             </div>
-        </div>
-        <div class="control-group">
-            <label class="control-label" for="description">Description</label>
-            <div class="controls">
-                <textarea rows="5" id="description" name="description"><?php echo $group->description; ?></textarea>
+            <div class="control-group">
+                <label class="control-label" for="description">Description</label>
+                <div class="controls">
+                    <textarea rows="5" id="description" name="description"><?php echo $group->description; ?></textarea>
+                </div>
             </div>
-        </div>
-    </fieldset>
-</form>
+        </fieldset>
+    </form>
+</section>
+<section>
+    <legend>
+        Files
+    </legend>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Link
+                </th>
+                <th>
+                    Downloads
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($group->downloads as $download): ?>
+                <tr>
+                    <td>
+                        <?php echo $download->name; ?>
+                    </td>
+                    <td>
+                        <?php echo $download->link; ?>
+                    </td>
+                    <td>
+                        <?php echo $download->number_of_downloads; ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</section>
