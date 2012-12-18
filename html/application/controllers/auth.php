ed42f49cd432da0a8bee60b06804cd92112a5e19
Ryan Sullivan <KayoticSully@gmail.com>
2012-12-18 16:33:42 -0500

    Added doc and empty login function

---
+++ html/application/controllers/auth.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Auth
+ * 
+ * @author      Ryan Sullivan
+ */
+class Auth extends MS2_Controller {
+    
+    public function login()
+    {
+        echo "login";
+    }
+}
+
+/* End of file home.php */
+/* Location: ./application/controllers/auth.php */
