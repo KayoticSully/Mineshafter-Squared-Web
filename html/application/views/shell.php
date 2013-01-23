fe83848e83cc53d9cbfec794a783b80e0f2e42b7
Ryan Sullivan <KayoticSully@gmail.com>
2013-01-23 03:36:51 -0500

    Second attempt to fix GA Cookie bug

--- html/application/views/shell.php
+++ html/application/views/shell.php
@@ -49,8 +49,8 @@
     <script type="text/javascript">
         var _gaq = _gaq || [];
         _gaq.push(['_setAccount', 'UA-24037493-2']);
-        _gaq.push(['_setDomainName', 'alpha.mineshaftersquared.com']);
-        _gaq.push(['_addIgnoredRef', 'alpha.mineshaftersquared.com']);
+        _gaq.push(['_setDomainName', 'mineshaftersquared.com']);
+        _gaq.push(['_addIgnoredRef', 'mineshaftersquared.com']);
         _gaq.push(['_trackPageview']);
         (function() {
         var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
