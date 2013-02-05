641d377d92a9805c19ace7c03a5849ece1ebc5a0
Ryan Sullivan <KayoticSully@gmail.com>
2013-02-05 05:29:49 -0500

    Update for tag popover

--- html/assets/javascript/skins.js
+++ html/assets/javascript/skins.js
@@ -8,7 +8,7 @@ function init() {
     $('#remove-from-library').on('click', remove_from_library);
     
     // control tags popover
-    if($('#tags').html().trim() != '') {
+    if($('#tags').find('label').size() <= 1) {
         $('#new-tag input[type=text]').popover('destroy');
     } else{
         $('#new-tag input[type=text]').popover('show');
