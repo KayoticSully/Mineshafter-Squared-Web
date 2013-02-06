53f941ffb50ba34bae15555b837ebcb59296aa9f
Ryan Sullivan <KayoticSully@gmail.com>
2013-02-06 01:21:08 -0500

    Added Info to homepage

--- html/application/views/home/index.php
+++ html/application/views/home/index.php
@@ -16,9 +16,44 @@
 </section>
 <section id="other">
     <h1>
-        Some Other Information
+        About Mineshafter Squared
     </h1>
-    <div style="height:200px;">
-        I will put some more information here sompoint before launch.
+    <div class="info_track">
+        <p>
+            Mineshafter Squared is an authentication and texture proxy for the
+            popular game <a href-"http://minecraft.net">Minecraft</a>
+            created by <a href="http://notch.tumblr.com/">Notch</a> of
+            <a href="http://mojang.com/">Mojang</a>. It gets in the
+            middle of the game clients and its outbound calls to the minecraft
+            servers. This allows my service to provide its own login system for
+            the game which enables players to try out the game before buying. My
+            authentication service is also online whenever the official
+            Minecraft server's go down. That is not as much a problem anymore,
+            but was very useful in the past.
+        </p>
+        <p>
+            The proxy also allows me to offer an amazing texture management
+            system that far outshines the official service. Users can upload
+            multiple textures to switch between, discover new textures, and
+            share them with their friends and the community.
+        </p>
+    </div>
+    <h1>
+        Origins
+    </h1>
+    <div class="info_track">
+        <p>
+            Mineshafter Squared is a bit of an odd name, this is because I based
+            my work off of the <a href="http://mineshafter.info/">Original
+            Mineshafter</a> and was trying to be clever. I built the Mineshafter
+            Squared authentication server and website from the ground up myself,
+            I only used the <a
+            href="https://github.com/download13/Mineshafter-Proxy">Mineshafter
+            proxy code</a> to help get me started. The creator of Mineshafter,
+            <a href="https://github.com/download13">download13</a>, also helped
+            me out along the way. I have changed much of his original code for
+            my own uses, but I would not have been able to make any of this
+            without that bit of help.
+        </p>
     </div>
 </section>
