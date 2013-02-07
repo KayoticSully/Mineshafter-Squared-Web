c66cc33454229b97713f8c71dd230c9728f4232e
Ryan Sullivan <KayoticSully@gmail.com>
2013-02-07 18:47:16 -0500

    Added Ad to Skin Page

--- html/application/views/skins/index.php
+++ html/application/views/skins/index.php
@@ -53,5 +53,17 @@
                 Uploaded on <?php echo date('F jS, Y', strtotime($skin->created_at)); ?>
             </div>
         </div>
+        <div>
+            <script type="text/javascript">
+                <!--
+                google_ad_client = "ca-pub-2130540909688027";
+                /* Mineshafter Squared Skin Page */
+                google_ad_slot = "9124239430";
+                google_ad_width = 250;
+                google_ad_height = 250;
+                //-->
+            </script>
+            <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+        </div>
     </div>
 </div>
