a35eaff3fc89803d66885fa9b627b72beffe2c2a
Ryan Sullivan <KayoticSully@gmail.com>
2013-02-08 19:51:09 -0500

    Took out commented code

--- html/application/views/downloads/html.php
+++ html/application/views/downloads/html.php
@@ -14,27 +14,4 @@
             </li>
         <?php endforeach; ?>
     </ul>
-    <?php /*
-    <article>
-        <header>
-            <?php echo $group->name; ?>
-        </header>
-        <div class="body download">
-            <div class="description">
-                <?php echo $group->description; ?>
-            </div>
-            <ul class="download links">
-                <?php foreach($group->downloads as $download): ?>
-                    <li>
-                        <a href="<?php echo $download->link; ?>">
-                            <?php echo $download->name; ?>
-                        </a>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
-        <footer>
-            Version <?php echo $group->version; ?>
-        </footer>
-    </article> */?>
 <?php endforeach; ?>
